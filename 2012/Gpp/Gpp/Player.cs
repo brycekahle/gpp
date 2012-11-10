5a51df0534463de04319f014f57981e7bbc8a4f1
Bryce Kahle <bkahle@gmail.com>
2012-11-10 12:47:31 -0600

    Add movement to Player. Add Projectile

--- 2012/Gpp/Gpp/Player.cs
+++ 2012/Gpp/Gpp/Player.cs
@@ -16,8 +16,10 @@ namespace Gpp
 
         private Vector2 _aimingVector;
 
-        private const float ChargeRate = 0.2f; // % /s
+        private const float ChargeRate = 1.0f; // m/s
         private const float AimingRotateRate = 1.5f; // radians / s
+        private const float MaxCharge = 5.0f;
+        private const float MovementSpeed = 1.0f; // m/s
 
         public Player(SupermassiveGame game, PlayerIndex controlIndex)
             : base(game)
@@ -34,9 +36,20 @@ namespace Gpp
 
         private void ReadSticks(GamePadState state, TimeSpan elapsedTime)
         {
-            if (!_isCharging) // can't move and charge at same time
+            var leftStick = state.ThumbSticks.Left.X;
+            var leftMovementVector = Vector2.TransformNormal(Heading, Matrix.CreateRotationZ((float)Math.PI / 2.0f));
+            var rightMovementVector = Vector2.TransformNormal(Heading, Matrix.CreateRotationZ((float)-Math.PI / 2.0f));
+
+            if (_isCharging || leftStick == 0)
             {
-                var leftStick = state.ThumbSticks.Left;
+                // cancel out left and right movement velocities
+                Velocity -= Vector2.Dot(Velocity, leftMovementVector) * leftMovementVector;
+                Velocity -= Vector2.Dot(Velocity, rightMovementVector) * rightMovementVector;
+            }
+            else
+            {
+                var movementVector = leftStick > 0 ? rightMovementVector : leftMovementVector;
+                Velocity += movementVector * (MovementSpeed * (float)elapsedTime.TotalSeconds);
             }
 
             var rightStickX = state.ThumbSticks.Right.X;
@@ -57,14 +70,17 @@ namespace Gpp
             else
             {
                 _isCharging = true;
-                _chargeAmount = Math.Min(_chargeAmount + (ChargeRate * (float)elapsedTime.TotalSeconds), 1.0f);
+                _chargeAmount = Math.Min(_chargeAmount + (ChargeRate * (float)elapsedTime.TotalSeconds), MaxCharge);
             }
         }
 
         private void Fire()
         {
+            var proj = new Projectile(Game, this, _aimingVector, _chargeAmount);
+            Game.GameObjects.Add(proj);
+
             _isCharging = false;
-            _chargeAmount = 0;
+            _chargeAmount = 1;
         }
 
         public override void Update(TimeSpan elapsedTime)
---
+++ 2012/Gpp/Gpp/Projectile.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gpp
+{
+    class Projectile : MovableObject
+    {
+        private Player _firingPlayer;
+
+        public Projectile(SupermassiveGame game, Player firingPlayer, Vector2 initialHeading, float firingPower)
+            : base(game)
+        {
+            _firingPlayer = firingPlayer;
+            Heading = initialHeading;
+            Velocity = Heading * firingPower;
+        }
+
+        public override void Update(TimeSpan elapsedTime)
+        {
+            base.Update(elapsedTime);
+        }
+
+        public override void Draw()
+        {
+            base.Draw();
+        }
+    }
+}
