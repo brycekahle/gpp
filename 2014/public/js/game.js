3d811c0a8027062d4a4718518e1105e89c1d8304
Bryce Kahle <bryce.kahle@mlssoccer.com>
2014-03-01 12:15:47 -0500

    Move camera, and keep player fixed to it

--- 2014/public/js/game.js
+++ 2014/public/js/game.js
@@ -1,8 +1,10 @@
 'use strict';
 
 window.onload = function() {
-  var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
+  var game = new Phaser.Game(800, 600, Phaser.CANVAS, '', { preload: preload, create: create, update: update, render: render });
   var player1, ship;
+
+  var cameraSpeed = 100.0; // 100x / second
 
   function preload() {
     player1 = game.load.spritesheet('player1', 'assets/player1.png', 100, 100);
@@ -11,14 +13,24 @@ window.onload = function() {
   function create() {
     ship = game.add.sprite(32, game.world.height / 2, 'player1');
     ship.body.collideWorldBounds = true;
-    
+
+    game.world.setBounds(0, 0, 80000, 600);
+    game.input.onDown.add(pauseToggle, this);
+
+    ship.fixedToCamera = true;
   }
 
   function update() {
-    
+    var xdiff = (cameraSpeed * (game.time.elapsed / 1000));
+    game.camera.x += xdiff;
   }
 
   function render() {
+    game.debug.renderCameraInfo(game.camera, 32, 32);
+    game.debug.renderSpriteCoords(ship, 32, 100);
+  }
 
+  function pauseToggle() {
+    game.paused = !game.paused;
   }
 };
