7ce78bd5d4afa5b29e84f066cb3b7dd70e9cf4a6
Ryan Versaw <ryan@versaw.com>
2014-03-01 11:13:03 -0600

    Getting a bit more in place with player updates

--- 2014/public/js/game.js
+++ 2014/public/js/game.js
@@ -1,8 +1,10 @@
 'use strict';
 
+// A little lame, but want this for easy debugging
+var player1, playerShip, game;
+
 window.onload = function() {
-  var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
-  var player1, playerShip;
+  game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
 
   function preload() {
     player1 = game.load.spritesheet('player1', 'assets/player1.png', 100, 100);
@@ -13,7 +15,7 @@ window.onload = function() {
   }
 
   function update() {
-    
+    playerShip.update();
   }
 
   function render() {
--- 2014/public/js/player-ship.js
+++ 2014/public/js/player-ship.js
@@ -1,5 +1,9 @@
 'use strict';
 function PlayerShip(game){
-	this.ship = game.add.sprite(32, game.world.height / 2, 'player1');
-    this.ship.body.collideWorldBounds = true;
+	this.sprite = game.add.sprite(32, game.world.height / 2, 'player1');
+    this.sprite.body.collideWorldBounds = true;
+
+    this.update = function(){
+    	this.sprite.x++;
+    }
 }
