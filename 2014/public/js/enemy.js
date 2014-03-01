b9fe68325bdd0c7755dd66a337b536ae8af31142
Bryce Kahle <bryce.kahle@mlssoccer.com>
2014-03-01 17:54:31 -0500

    Enemy death explosions

--- 2014/public/js/enemy.js
+++ 2014/public/js/enemy.js
@@ -13,3 +13,13 @@ Enemy.prototype.constructor = Enemy;
 Enemy.prototype.update = function () {
   this.body.velocity.y = Math.sin(2 * Math.PI * (this.game.time.now / 1000) + (this.phase / 360) * (2*Math.PI)) * 200;
 };
+
+Enemy.prototype.kill = function() {
+  Phaser.Sprite.prototype.kill.call(this);
+  var deathEmitter = this.game.add.emitter(this.x, this.y, 50);
+  deathEmitter.makeParticles('deathbits', 0, 100);
+  deathEmitter.minParticleSpeed.setTo(-800, -800);
+  deathEmitter.maxParticleSpeed.setTo(800, 800);
+  deathEmitter.gravity = 0;
+  deathEmitter.start(true, 500, 0, 50);
+};
