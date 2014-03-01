3a5ba5e0ad64712e6cf0a44684d1ab59cd39c820
Bryce Kahle <bryce.kahle@mlssoccer.com>
2014-03-01 11:35:45 -0500

    Add player1 square sprite

--- 2014/public/js/game.js
+++ 2014/public/js/game.js
@@ -1,15 +1,24 @@
+'use strict';
+
 window.onload = function() {
-  var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+  var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
+  var player1, ship;
 
   function preload() {
-
+    player1 = game.load.spritesheet('player1', 'assets/player1.png', 100, 100);
   }
 
   function create() {
-
+    ship = game.add.sprite(32, game.world.height / 2, 'player1');
+    ship.body.collideWorldBounds = true;
+    
   }
 
   function update() {
     
+  }
+
+  function render() {
+
   }
 };
